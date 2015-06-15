b40c5e4d23044d00abe4d64f94af43cb73474270
TAKANO Mitsuhiro <takano32@gmail.com>
2015-06-15 23:22:29 +0900

    add hello.c

--- hello.c
+++ hello.c
@@ -1,7 +1,16 @@
 #include <stdio.h>
+#include <ruby.h>
 
+VALUE cHello;
+
+VALUE hello_say(VALUE self) {
+	puts("hello.");
+	return self;
+}
 
 void Init_hello() {
-	puts("Hello.");
+	cHello = rb_define_class("Hello", rb_cObject);
+	rb_define_method(cHello, "say", RUBY_METHOD_FUNC(hello_say), 0);
 }
+
 
